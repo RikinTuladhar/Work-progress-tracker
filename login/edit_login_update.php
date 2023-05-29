4cd267f36a656df618a39790386b7d3796329a26
Rikin07 <110732947+Senyadt2@users.noreply.github.com>
2023-05-29 21:46:00 +0545

    fileupload in folder

--- login/edit_login_update.php
+++ login/edit_login_update.php
@@ -1,10 +1,10 @@
 <!-- SQL command for updating data that came from edit_login.php  -->
 
 <?php
-    if ( $_SERVER['REQUEST_METHOD']=='GET' && realpath(__FILE__) == realpath( $_SERVER['SCRIPT_FILENAME'] ) ) {
-        header( 'HTTP/1.0 403 Forbidden', TRUE, 403 );
-        die ("<h2>Access Denied!</h2> This file is protected and not available to public.");
-        }
+    // if ( $_SERVER['REQUEST_METHOD']=='GET' && realpath(__FILE__) == realpath( $_SERVER['SCRIPT_FILENAME'] ) ) {
+    //     header( 'HTTP/1.0 403 Forbidden', TRUE, 403 );
+    //     die ("<h2>Access Denied!</h2> This file is protected and not available to public.");
+    //     }
    $conn = mysqli_connect("localhost","root","","workprogresstracker");
     $id = $_POST['id'];
     $username = $_POST['username'];
