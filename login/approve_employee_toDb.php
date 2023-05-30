b2503f5fe5d5d4ed7e24a9f1e8687c8618fb2377
Rikin07 <110732947+Senyadt2@users.noreply.github.com>
2023-05-30 13:15:39 +0545

    Update approve_employee_toDb.php

--- login/approve_employee_toDb.php
+++ login/approve_employee_toDb.php
@@ -5,7 +5,7 @@ $conn =mysqli_connect("localhost","root","","workprogresstracker");
 if(isset($_GET['id']))
 {
 $id = $_GET['id'];
-$sqllog = "select * from login where id =".$id;
+$sqllog = "select * from login where id =$id";
 $resultlog = mysqli_query($conn,$sqllog);
 if($resultlog->num_rows > 0)
 {
@@ -19,18 +19,21 @@ if($resultlog->num_rows > 0)
 else{
     echo "no data";
 }
-
-$sql = "INSERT INTO `employee`(`eid`, `emp_name`,`e_pw`,`emp_email`) VALUES ('$id','$username','$password','$email')";
+$sql = "INSERT INTO `employee`(`emp_name`, `emp_email`,`e_pw`) VALUES ('$username','$email','$password')";
 try{
 if(mysqli_query($conn,$sql))
 {
     header('location:displaydata.php');
     // var_dump($username);
 }
+else {
+    mysqli_error($conn);
 }
-catch(Exception )
+}
+catch(\Exception $e)
 {   
-    echo "<script> alert('Cannot Approve Twice') </script>";
+   // echo "<script> alert('Cannot Approve Twice') </script>";
+   echo "error";
 }
 
 }
