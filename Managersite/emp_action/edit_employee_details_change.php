dff6f11c708cf8e5915a29d9a74b709c17d182e1
Rikin07 <110732947+Senyadt2@users.noreply.github.com>
2023-06-02 23:10:39 +0545

    Update edit_employee_details_change.php

--- Managersite/emp_action/edit_employee_details_change.php
+++ Managersite/emp_action/edit_employee_details_change.php
@@ -4,19 +4,23 @@ if($con->connect_error)
 {
     die($con->connect_error);
 }
-$id = $_POST['eid'];
+$id = $_POST['id'];
 if(empty($id))
 {
     header('location:http://localhost/work-progress-tracker/Work-progress-tracker/Managersite/employee_homepage.php');
+    // echo "no data";
 }
+else
+{
 $Name=$_POST['Name'];
 $Email=$_POST['Email'];
 $Lastname=$_POST['Lastname'];
 $Phone = $_POST['Phone'];
 $password=$_POST['password'];
-$sql = "UPDATE employee set emp_name='$Name',emp_email='$Email',emp_lastname='$Lastname',emp_phone='$Phone',e_pw='$password'";
+$sql = "UPDATE employee set emp_name='$Name',emp_email='$Email',emp_lastname='$Lastname',emp_phone='$Phone',e_pw='$password' where eid = $id";
 $result=mysqli_query($con,$sql);
 if($result)
 {
     header('location:http://localhost/work-progress-tracker/Work-progress-tracker/Managersite/employee_homepage.php');
+}
 }
