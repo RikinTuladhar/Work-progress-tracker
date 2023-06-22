3e527c0ba3977d42337e9796c39181950b6a5605
Rikin07 <110732947+Senyadt2@users.noreply.github.com>
2023-06-22 21:20:23 +0545

    update on substr()

--- Managersite/ajaxfile/sel.php
+++ Managersite/ajaxfile/sel.php
@@ -5,7 +5,7 @@ $conn = mysqli_connect("localhost","root","","workprogresstracker");
 
 $val=$_POST['data'];
 
-$sql_task = "select task_id,task_title,emp_name,status,start_date,end_date,task_description from tasks LEFT OUTER JOIN employee on tasks.e_id =employee.eid ";
+$sql_task = "select task_id,task_title,emp_email,status,start_date,end_date,task_description from tasks LEFT OUTER JOIN employee on tasks.e_id =employee.eid ";
 
 $result_task = mysqli_query($conn,$sql_task);
 
@@ -34,8 +34,8 @@ for($a=0;$a<$val;$a++){
     <td>".$rows['status']."</td>
     <td>".$rows['start_date']."</td>
     <td>".$rows['end_date']."</td>
-    <td>".$rows['task_description']."</td>
-    <td>".$rows['emp_name']."</td>
+    <td>" .substr($rows['task_description'],0,20)."</td>
+    <td>".$rows['emp_email']."</td>
     <td> <a href ='task_action/tasskedit.php?task_id=".$rows["task_id"]."'>Edit</a></td>
     <td> <a href ='task_action/tasskdelete.php?task_id=".$rows["task_id"]."'>Delete</a></td>
     <tr>";
--- Managersite/ajaxfile/sel2.php
+++ Managersite/ajaxfile/sel2.php
@@ -7,7 +7,7 @@ $conn = mysqli_connect("localhost","root","","workprogresstracker");
 
 
 // $sql_task = "select * from tasks";
-$sql_task = "select task_id,task_title,emp_name,status,start_date,end_date,task_description from tasks LEFT OUTER JOIN employee on tasks.e_id =employee.eid ";
+$sql_task = "select task_id,task_title,emp_email,status,start_date,end_date,task_description from tasks LEFT OUTER JOIN employee on tasks.e_id =employee.eid ";
 $result_task = mysqli_query($conn,$sql_task);
 $idnum = 1;
 echo "<tr>
@@ -36,8 +36,8 @@ if($result_task->num_rows > 0)
             <td>".$rows['status']."</td>
             <td>".$rows['start_date']."</td>
             <td>".$rows['end_date']."</td>
-            <td>".$rows['task_description']."</td>
-            <td>".$rows['emp_name']."</td>
+            <td>" .substr($rows['task_description'],0,20)."</td>
+            <td>".$rows['emp_email']."</td>
             <td> <a href ='task_action/tasskedit.php?task_id=".$rows["task_id"]."'>Edit</a></td>
             <td> <a href ='task_action/tasskdelete.php?task_id=".$rows["task_id"]."'>Delete</a></td>
             <tr>";
