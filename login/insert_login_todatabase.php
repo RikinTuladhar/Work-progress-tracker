75f25880e5b42eb2f269736670d4decae295f0e1
Rikin07 <110732947+Senyadt2@users.noreply.github.com>
2023-08-20 11:54:47 +0545

    Update insert_login_todatabase.php

--- login/insert_login_todatabase.php
+++ login/insert_login_todatabase.php
@@ -14,7 +14,14 @@ $username=$_POST['username'];
 $password=$_POST['password'];
 $email=$_POST['email'];
 
-$sql= "select * from login where email = '$email'";
+// $sql= "SELECT l.email ,m.m_email , e.emp_email  FROM  login l  INNER JOIN manager m on l.email = m.m_email INNER JOIN employee e on l.email = e.emp_email  where l.email = '$email' OR m.m_email = '$email' OR e.emp_email = '$email' ";
+$sql = "SELECT login.email from login  where email = '$email' 
+UNION ALL 
+SELECT manager.m_email from manager where m_email = '$email' 
+UNION ALL
+Select employee.emp_email from employee where emp_email = '$email'";
+
+
 $result = mysqli_query($conn,$sql);
 $count = mysqli_num_rows($result);
 if($count > 0 )
