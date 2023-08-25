0d6bffe65aad54b15fd698bc7b73095e48b41d78
Rikin07 <110732947+Senyadt2@users.noreply.github.com>
2023-08-25 19:00:57 +0545

    php mailer

--- login/insert_login_todatabase.php
+++ login/insert_login_todatabase.php
@@ -1,6 +1,12 @@
 <?php
 // require "../database/crud.php";
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
 
+      
+require 'phpmailer/src/Exception.php';
+require 'phpmailer/src/PHPMailer.php';
+require 'phpmailer/src/SMTP.php';
 
 $conn = mysqli_connect("localhost","root","","workprogresstracker");
 
@@ -15,10 +21,10 @@ $password=$_POST['password'];
 $email=$_POST['email'];
 
 // $sql= "SELECT l.email ,m.m_email , e.emp_email  FROM  login l  INNER JOIN manager m on l.email = m.m_email INNER JOIN employee e on l.email = e.emp_email  where l.email = '$email' OR m.m_email = '$email' OR e.emp_email = '$email' ";
-$sql = "SELECT login.email from login  where email = '$email' 
-UNION ALL 
-SELECT manager.m_email from manager where m_email = '$email' 
-UNION ALL
+// SELECT login.email from login  where email = '$email'  UNION ALL 
+$sql = "
+SELECT login.email from login  where email = '$email'  UNION ALL 
+SELECT manager.m_email from manager where m_email = '$email'  UNION ALL
 Select employee.emp_email from employee where emp_email = '$email'";
 
 
@@ -31,19 +37,35 @@ if($count > 0 )
     alert("Email exist");
         window.location.href = "http://localhost/work-progress-tracker/Work-progress-tracker/login/login.html";
 </script>
-
 <?php
 } 
 else{
     $insert = "insert into login (username,	password,email)values('$username','$password','$email')";
    if( mysqli_query($conn,$insert))
    {
-    ?>
-    <script> alert("Inserted");
-window.location.href = "http://localhost/work-progress-tracker/Work-progress-tracker/login/login.html";</script>
+        //send email
+            $mail = new PHPMailer(true);
+            $mail->isSMTP();
+            $mail->Host = 'smtp.gmail.com';
+            $mail-> SMTPAuth = true;
+            $mail->Username = 'abc73357240@gmail.com';    //your gmail
+            $mail->Password = 'xzngkworkmmjtewo';  //your password
+            $mail->SMTPSecure = 'ssl'; //connection secured 
+            $mail->Port = 465;
+            $mail->setFrom('workprogresstracker@gmail.com');
+            $mail->addAddress($email); //send to
+            $mail->isHTML(true);
+            $mail->Subject = "Account Created!";
+            $mail->Body = "We have created your account at workprogresstracker.com ";
+            $mail->send();
+            
+            ?>
+            <script>
+                alert("Sent Successfully");
+                alert("Inserted");
+                window.location.href = "http://localhost/work-progress-tracker/Work-progress-tracker/login/login.html";</script>
     <?php
    }
 }
 $conn->close();
-
 ?>
