0b2f9e9603c389482f9725962e345e43b052b104
Rikin07 <110732947+Senyadt2@users.noreply.github.com>
2023-08-27 10:06:13 +0545

    v.0.7
    Updated on landing page

--- login/verification_login.php
+++ login/verification_login.php
@@ -2,82 +2,76 @@
 
 // if(isset($_GET['vername']) && isset($_GET['verpassword']))
 // {
-    session_start();
-$emailname=$_GET['vername'];
-$password=$_GET['verpassword'];
+session_start();
+$emailname = $_GET['vername'];
+$password = $_GET['verpassword'];
 $count = 0;
 // var_dump($emailname);
 
-$con = mysqli_connect("localhost","root","","workprogresstracker");
-if($con->connect_error)
-{
-    die("connection error".$con->connect_error);
+$con = mysqli_connect("localhost", "root", "", "workprogresstracker");
+if ($con->connect_error) {
+    die("connection error" . $con->connect_error);
 }
 $sql = "SELECT * FROM employee";
-$result = mysqli_query($con,$sql);
+$result = mysqli_query($con, $sql);
 
 
 
 
-if($result->num_rows > 0)
-{
-    while($row = $result->fetch_assoc())
-    {
-        
-        if($row['emp_email'] == $emailname && $row['e_pw'] == $password)
-        {
-            
+if ($result->num_rows > 0) {
+    while ($row = $result->fetch_assoc()) {
+
+        if ($row['emp_email'] == $emailname && $row['e_pw'] == $password) {
+
             $_SESSION['username'] = $row['emp_name'];
-            $_SESSION['id']=$row['eid'];
+            $_SESSION['id'] = $row['eid'];
+
+
             // var_dump($row['eid'] );
             $count = 1;
             // if(isset($_SESSION['username']))
-           
-            ?>
-            <script>alert("Log in success <?php echo $_SESSION['id']?>");
-          window.location.href="http://localhost/work-progress-tracker/Work-progress-tracker/Employeesite/home_employee.php?"</script>
-          
-         <?php
+
+?>
+            <script>
+                alert("Log in success <?php echo $_SESSION['id'] ?>");
+                window.location.href = "http://localhost/work-progress-tracker/Work-progress-tracker/Employeesite/home_employee.php?"
+            </script>
+
+        <?php
 
         }
-        
-    } 
-    
-    
-    
+    }
 }
 $managersql = "SELECT * FROM manager";
-$managercheck = mysqli_query($con,$managersql);
+$managercheck = mysqli_query($con, $managersql);
 
-if($managercheck->num_rows > 0)
-{
-    while($rows = $managercheck->fetch_assoc())
-    if(($rows['m_email'] ==  $emailname &&  $rows['m_pw'] == $password))
-    {
-        // echo $rows['m_name'];
-        $_SESSION['username'] = $rows['m_name'];
-        $_SESSION['id']=$rows['m_id'];
-        $count = 1;
+if ($managercheck->num_rows > 0) {
+    while ($rows = $managercheck->fetch_assoc())
+        if (($rows['m_email'] ==  $emailname &&  $rows['m_pw'] == $password)) {
+            // echo $rows['m_name'];
+            $_SESSION['username'] = $rows['m_name'];
+            $_SESSION['id'] = $rows['m_id'];
+            $count = 1;
         ?>
-    <script>alert("Log in success for manager");
-      window.location.href="http://localhost/work-progress-tracker/Work-progress-tracker/Managersite/manager_homepage.php?<?php echo $_SESSION['username'];?>"</script>
+        <script>
+            alert("Log in success for manager");
+            window.location.href = "http://localhost/work-progress-tracker/Work-progress-tracker/Managersite/manager_homepage.php?<?php echo $_SESSION['username']; ?>"
+        </script>
 
-     <?php
-        
-    }
+<?php
 
+        }
 }
 
 
-if($count == 0)
-{
-header('location:login.html');
+if ($count == 0) {
+    header('location:login.html');
 }
 
 
 
 // }
 // else{
-    // echo "Value not received";
-    // }
+// echo "Value not received";
+// }
 ?>
